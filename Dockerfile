6d9ca752c37e9eae4d666e4e83cf43d0f2cffb1b
Vanessa Mohr <vanessa.mohr@standardbank.co.za>
2025-05-13 05:07:33 +0200

    Added Dockerfile to use AWS Lambda Emulator for local testing

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Use AWS Lambda base image for Python
+FROM public.ecr.aws/lambda/python:3.13
+
+# Set working directory
+WORKDIR /var/task
+
+# Copy your app code into the container
+COPY . .
+
+# Copy Poetry files and install dependencies
+COPY pyproject.toml poetry.lock ./
+RUN pip install poetry && poetry config virtualenvs.create false && poetry install
+
+# Set the Lambda entrypoint (e.g. main.lambda_handler)
+CMD ["my_python_playground.main.lambda_handler"]
--- my_python_playground/main.py
+++ my_python_playground/main.py
@@ -1,5 +1,6 @@
-def main():
-    # Read the README, then:
-    # run "run-me" in the terminal once you have activated 
-    # your virtual environment and installed your project
+def lambda_handler(event, context):
+    # You can invoke your Lambda locally using the [AWS Lambda Runtime Interface Emulator (RIE)].
+    # Build and run the Docker image. Then, invoke it locally using: 
+    # curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+    # You should now see 'Hello World!' in the container logs.
     print("Hello World!")
